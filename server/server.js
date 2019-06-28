d95ba799084302916b0191474f255981dbd0afc3
The Goose <trevordammon@gmail.com>
2019-06-28 15:56:56 -0500

    changing port

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const app = require('express');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-
-server.listen(process.env.PORT || 8000, () => console.log('connected to port 8000!'));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`connected to ${PORT}!`));
 
 
 
