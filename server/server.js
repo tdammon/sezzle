03253ce0506c7b0e89ccd6b514bd6358ec40fab1
The Goose <trevordammon@gmail.com>
2019-06-28 16:24:51 -0500

    fixing server issue

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-const app = require('express');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.use(express.static('build'));
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
