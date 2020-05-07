1a8950493463534cef821c7b343b9d510b1bfa52
The Goose <trevordammon@gmail.com>
2020-05-07 00:19:58 -0500

    .

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
-// const socket = io();
-const socket = io('http://localhost:8000');
+const socket = io();
+// const socket = io('http://localhost:8000');
 
 const configureSocket = dispatch => {
   
